81d3d60ad797c8a962b9249b0770c3ddc73c6648
Draven Nova <draven77medina@hotmail.com>
2025-07-06 22:10:04 -0700

    small typo in dev stage from localhost to actual api link

--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -24,7 +24,7 @@ export default function SignupForm() {
   e.preventDefault();
 
   try {
-    const response = await fetch('http://localhost:3001/auth/signup', {
+    const response = await fetch('https://oracleutilityapp.onrender.com/auth', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
